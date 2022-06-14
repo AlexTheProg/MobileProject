5986a9c0849919e43991f7a1ba0d312babf438ab
Alexandru Contu <44123566+AlexTheProg@users.noreply.github.com>
2022-06-14 23:08:08 +0300

    google maps commit

---
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -0,0 +1,75 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { PlaceLocation } from 'src/app/places/location.model';
+import { environment } from 'src/environments/environment';
+import { MapModalComponent } from '../../map-modal/map-modal.component';
+
+@Component({
+  selector: 'app-location-picker',
+  templateUrl: './location-picker.component.html',
+  styleUrls: ['./location-picker.component.scss'],
+})
+export class LocationPickerComponent implements OnInit {
+
+  @Output() locationPick = new EventEmitter<PlaceLocation>();
+  selectedLocationImage: string;
+
+
+  constructor(private modalCtrl: ModalController, private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  onPickLocation() {
+    this.modalCtrl
+      .create({
+        component: MapModalComponent,
+      })
+      .then((modalEl) => {
+        modalEl.onDidDismiss().then((modalData) => {
+          if (!modalData.data){
+            return;
+          }
+          const pickedLocation: PlaceLocation = {
+            lat: modalData.data.lat,
+            lng: modalData.data.lng,
+            address: null,
+            staticMapImageUrl: null
+          };
+          this.getAddress(modalData.data.lat, modalData.data.lng).pipe(
+            switchMap(address => {
+              pickedLocation.address = address;
+              return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14));
+            })
+          ).subscribe(mapUrl => {
+            pickedLocation.staticMapImageUrl = mapUrl;
+            this.selectedLocationImage = mapUrl;
+            this.locationPick.emit(pickedLocation);
+          });
+        });
+        modalEl.present();
+      });
+  }
+
+  private getAddress(lat: number, lng: number) {
+    return this.http
+      .get(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${environment.GOOGLE_MAPS_API_KEY}`
+      )
+      .pipe(
+        map((geoData: any) => {
+          if (!geoData || !geoData.results || geoData.results.length === 0) {
+            return null;
+          }
+          return geoData.results[0].formatted_address;
+        })
+      );
+  }
+
+  private getMapImage(lat: number, lng: number, zoom: number){
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&maptype=roadmap
+    &markers=color:red%7Clabel:Places%7C${lat},${lng}&key=${environment.GOOGLE_MAPS_API_KEY}`;
+  }
+}
